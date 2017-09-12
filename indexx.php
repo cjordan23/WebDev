7b42c2923ef99376b2055ce99277cca3a5cb8d0e
cjordan23 <christopherjody@gmail.com>
2017-09-12 16:02:17 -0700

    Commit kedua

--- indexx.php
+++ indexx.php
@@ -16,7 +16,7 @@
     <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous"> -->
 
     <!--Local Style CSS  -->
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="D:/MyGIT/jordanpayangan/jordan/css/style.css">
     <title>jordan</title>
   </head>
   <body>
@@ -24,9 +24,9 @@
         <div class="navbar-fixed">
           <nav>
             <div class="nav-wrapper">
-              <a href="index.html" class="brand-logo">Logo</a>
+              <a href="#" class="brand-logo">Logo</a>
               <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a href="login.html">Login</a></li>
+                <li><a href="sass.html">Sass</a></li>
                 <li><a href="badges.html">Components</a></li>
                 <li><a href="collapsible.html">JavaScript</a></li>
               </ul>
@@ -49,41 +49,11 @@
             <div id="separation2">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque perspiciatis, quis harum tempore repellendus vitae eaque quae reprehenderit ipsam ducimus qui, dicta? Nemo deleniti voluptas, illum doloremque soluta ea ipsam!
             </div>
-            <div id="img3">
-              <div class="row">
-                <div class="col s4">
-                  <h2 class="learn">
-                    LEARN
-                  </h2>
-                  <p class="text1">
-                    LEARN
-                  </p>
-                </div>
-                <div class="col s4">
-                  <h2 class="test">
-                    TEST
-                  </h2>
-                  <p class="text2">
-                    TEST
-                  </p>
-                </div>
-                <div class="col s4">
-                  <h2 class="deploy">
-                    DEPLOY
-                  </h2>
-                  <p class="text3">
-                    DEPLOY
-                  </p>
-                </div>
-              </div>
-            </div>
           </div>
         </div>
         <div id="footerID" class="footer">
           <div class="row">
-            <div class="col s4">
-              SATU
-            </div>
+            <div class="col s4">SATU</div>
             <div class="col s4">DUA</div>
             <div class="col s4">TIGA</div>
           </div>
@@ -100,5 +70,36 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
     -->
-  </body>
+  <!-- Code injected by live-server -->
+<script type="text/javascript">
+	// <![CDATA[  <-- For SVG support
+	if ('WebSocket' in window) {
+		(function() {
+			function refreshCSS() {
+				var sheets = [].slice.call(document.getElementsByTagName("link"));
+				var head = document.getElementsByTagName("head")[0];
+				for (var i = 0; i < sheets.length; ++i) {
+					var elem = sheets[i];
+					head.removeChild(elem);
+					var rel = elem.rel;
+					if (elem.href && typeof rel != "string" || rel.length == 0 || rel.toLowerCase() == "stylesheet") {
+						var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, '');
+						elem.href = url + (url.indexOf('?') >= 0 ? '&' : '?') + '_cacheOverride=' + (new Date().valueOf());
+					}
+					head.appendChild(elem);
+				}
+			}
+			var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
+			var address = protocol + window.location.host + window.location.pathname + '/ws';
+			var socket = new WebSocket(address);
+			socket.onmessage = function(msg) {
+				if (msg.data == 'reload') window.location.reload();
+				else if (msg.data == 'refreshcss') refreshCSS();
+			};
+			console.log('Live reload enabled.');
+		})();
+	}
+	// ]]>
+</script>
+</body>
 </html>
